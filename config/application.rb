997ff7757c225cfbb4dd599c174d0dc3439b2e1b
makino <makino@bunchosha.com>
2015-10-26 01:01:41 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Addb1025
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.enabled = true
   end
 end
