f40ba5bba895d088ab7b49c10b98b89457dc7f15
makino <makino@bunchosha.com>
2015-10-25 22:24:45 +0900

    test

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,8 +4,12 @@ working_directory @app_path + "/current"
 worker_processes 2
 preload_app true
 timeout 30
-listen "/tmp/unicorn.sock", :backlog => 64
-pid "/var/www/addb1025/shared/tmp/pids/unicorn.pid"
+
+
+app_path = "/var/www/addb1025"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/current/tmp/unicorn.pid"
+
 
 stderr_path "#{@app_path}/log/unicorn.stderr.log"
 stdout_path "#{@app_path}/log/unicorn.stdout.log"
