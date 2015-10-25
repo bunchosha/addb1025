a0542daa93cd5e63248ebf1c3c0bdf734491e66d
makino <makino@bunchosha.com>
2015-10-25 21:32:26 +0900

    test

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,11 +3,11 @@
 set :rails_env, "staging"
 set :unicorn_rack_env, "staging"
 
-role :app, %w{ec2-user@54.199.154.48} # 実際のサーバのIPアドレスを指定します。
-role :web, %w{ec2-user@54.199.154.48}
-role :db, %w{ec2-user@54.199.154.48}
+role :app, %w{ec2-user@54.178.197.128} # 実際のサーバのIPアドレスを指定します。
+role :web, %w{ec2-user@54.178.197.128}
+role :db, %w{ec2-user@54.178.197.128}
 
-server '54.199.154.48', user: 'ec2-user', roles: %w{web app db}
+server '54.178.197.128', user: 'ec2-user', roles: %w{web app db}
 
  
 set :ssh_options, {
