6b9d04e9856b5fb2f18243c1b39b4d3485321921
makino <makino@bunchosha.com>
2015-10-31 17:35:53 +0900

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
- before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
  
   def new
   	@user = User.new
@@ -7,6 +9,10 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
@@ -22,6 +28,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   def update
@@ -41,6 +53,16 @@ class UsersController < ApplicationController
     end
 
     def signed_in_user
+      store_location
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?([user])
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
