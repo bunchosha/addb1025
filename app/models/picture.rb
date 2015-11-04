cad73556a6b81a1692ad7d389ec66f1eea243215
makino <makino@bunchosha.com>
2015-11-04 22:13:41 +0900

    test

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,10 +3,10 @@ class Picture < ActiveRecord::Base
 
 
    has_attached_file :pic , styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-                :path => "#{Rails.root}/public/photo_attachment/:id/:style/:basename.:extension",
+	         	:path => "#{Rails.root}/public/photo_attachment/:id/:style/:basename.:extension",
                 :url => "/photo_attachment/:id/:style/:basename.:extension"
 
-  validates_attachment_size :pic, :less_than => 5.megabytes
+  validates_attachment_size :pic, :less_than => 3.megabytes
   validates_attachment_content_type :pic, :content_type => ['image/jpeg', 'image/png','image/gif']
 
 end
